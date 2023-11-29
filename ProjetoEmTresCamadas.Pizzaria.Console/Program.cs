27a258b5679186795e613d4d29ae5b8c7feb8526
George <george.hamann@gmail.com>
2023-11-28 21:36:14 -0300

    Projetos camada 1 e 2

---
+++ ProjetoEmTresCamadas.Pizzaria.Console/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Bem vindo a nossa pizaria");
+Console.WriteLine("Gostaria de uma pizza, S para sim e N para não?");
+var resposta = Console.ReadLine();
+Console.WriteLine(resposta);
+
+Console.WriteLine("Fim");
+
---
+++ ProjetoEmTresCamadas/ProjetoEmTresCamadas.Pizzaria.RegraDeNegocio/Pizza.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoEmTresCamadas.Pizzaria.RegraDeNegocio
+{
+    public enum TamanhoDePizza
+    {
+        Pequena = 0,
+        Media = 1,
+        Grande = 2
+    }
+
+    public class Pizza
+    {
+        public string Sabor { get; set; }
+        public TamanhoDePizza TamanhoDePizza { get; set; }
+        public string Descricao { get; set; }
+        public Pizza(){}
+        public Pizza CriarPizza
+            (string sabor, 
+            TamanhoDePizza tamanhoDePizza,
+            string descricao = "")
+        {
+            Sabor = sabor;
+            this.TamanhoDePizza = tamanhoDePizza;
+            if (string.IsNullOrEmpty(descricao)) {
+                Descricao = descricao;
+            }
+            return this;
+        }
+    }
+}
