7cd14477c6d13d692298d792b998050f7acb9745
George Hamann <george.hamann@gmail.com>
2023-12-01 20:17:44 -0300

    Exemplos aula collections e generics

---
+++ Aula03/Aula03/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+Aula03.Stack<DateTime> stackDate = new Aula03.Stack<DateTime>();
+
+stackDate.Push(DateTime.Now);
+stackDate.Push(
+    new DateTime(2023, 12, 01, 19, 12, 00));
+
+Aula03.Stack<string> stackString = new Aula03.Stack<string>();
+
+stackString.Push("Ola");
+stackString.Push("Generico");
+
+var a = stackDate.data[0];
+var b = stackDate.data[1];
+
+Aula03.StackMethod.Swap<DateTime>(ref a, ref b);
+
+var a1 = stackString.data[0];
+var b1 = stackString.data[1];
+
+Aula03.StackMethod.Swap<string>(ref a1, ref b1);
+
+
+List<string> colecaoDePalavras 
+    = new List<string>();
+
+colecaoDePalavras.Add("Ola");
+colecaoDePalavras.Remove("Ola");
+
+Dictionary<string, string> keyValuePairs 
+    = new Dictionary<string, string>();
---
+++ Aula03/Aula03/Stack.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula03
+{
+    public class Stack<T>
+    {
+        public int position { get; set; }
+        public T[] data = new T[100];
+
+        public void Push(T obj) => data[position++] = obj;
+        public T Pop() => data[--position];
+
+    }
+
+    public class StackMethod
+    {
+        public static void Swap<U>(ref U a, ref U b)
+        {
+            U temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+
+}
